0c9a712dad8290b3835548e70f6e0ede2e15fdc5
Martin Klepsch <martinklepsch@googlemail.com>
2021-05-08 14:14:46 +0200

    make build also run deploy + clean up any previous arc hydration

--- trying-arc/build.sh
+++ trying-arc/build.sh
@@ -2,6 +2,12 @@
 
 npx shadow-cljs release get-nanoid get-random get-another-nanoid
 
+# clean up from potential arc hydration process
+for FUNCTION_DIR in src/http/*
+do
+  rm -rf $FUNCTION_DIR/node_modules
+done
+
 for FUNCTION_DIR in src/http/*
 do
   mv $FUNCTION_DIR/index.js $FUNCTION_DIR/input.js
@@ -10,7 +16,8 @@ done
 
 for FUNCTION_DIR in src/http/*
 do
-  rm -rf $FUNCTION_DIR/node_modules/@architect
   rm $FUNCTION_DIR/index.js.map
   rm $FUNCTION_DIR/input.js
 done
+
+arc deploy --no-hydrate
